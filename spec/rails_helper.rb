b8cf46159082ea406f8cbb558c67d148fe82447c
Sierra Tucker <sierra.tucker121@gmail.com>
2022-01-17 14:16:15 -0700

    Test: mock any isntance of HolidayService

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,14 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
+
+  config.before :each do
+    mock_holidays = [{}, {}, {}]
+
+    allow_any_instance_of(HolidayService).to receive(:upcoming_us_holidays).and_return(mock_holidays)
+  end 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
